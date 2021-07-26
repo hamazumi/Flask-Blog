c90b733f606aa528b7745ce7907c0f9323e04fdb
hamazumi <khamamoto12@gmail.com>
2021-07-26 15:07:01 -0700

    added templates for jinja

--- blog.py
+++ blog.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'KEN test',
+        'title': 'TEST Post 1',
+        'content': 'TEST content 1',
+        'date_posted': 'July 26, 2021'
+    },
+    {
+        'author': 'JESS test',
+        'title': 'TEST Post 2',
+        'content': 'TEST content 2',
+        'date_posted': 'July 26, 2021'
+    },
+    {
+        'author': 'BUDDHA test',
+        'title': 'TEST Post 3',
+        'content': 'TEST content 3',
+        'date_posted': 'July 26, 2021'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return "<h1>Testing Homepage 🤙<h1/>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>About Page 💩<h1/>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
