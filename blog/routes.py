b6b2bde289c84a03e60bd4127e70dce0b0fa6964
hamazumi <khamamoto12@gmail.com>
2021-07-27 10:38:21 -0700

    working update and delete routes

--- blog/routes.py
+++ blog/routes.py
@@ -1,34 +1,15 @@
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, abort
 from blog import app, db
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, PostForm
 from blog.models import User, Post
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
-posts = [
-    {
-        'author': 'KEN test',
-        'title': 'TEST Post 1',
-        'content': 'TEST content 1',
-        'date_posted': 'July 26, 2021'
-    },
-    {
-        'author': 'JESS test',
-        'title': 'TEST Post 2',
-        'content': 'TEST content 2',
-        'date_posted': 'July 26, 2021'
-    },
-    {
-        'author': 'BUDDHA test',
-        'title': 'TEST Post 3',
-        'content': 'TEST content 3',
-        'date_posted': 'July 26, 2021'
-    }
-]
 
 # APP ROUTES
 @app.route('/')
 @app.route('/home')
 def homepage():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -63,9 +44,54 @@ def login():
             login_user(user, remember = form.remember.data)
             return redirect(url_for('homepage'))
     return render_template('login.html', title='Login', form=form)
-    
+
 
 @app.route('/logout')
 def logout():
     logout_user()
+    return redirect(url_for('homepage'))
+
+@app.route('/post/new', methods = ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('homepage'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route('/post/<int:post_id>/update', methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        # No need to db add because you are updating db
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        return redirect(url_for('post', post_id=post.id))
+    # Below fills out form with current data
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Edit Post', form=form, legend='Edit Post')
+
+@app.route('/post/<int:post_id>/delete', methods = ['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
     return redirect(url_for('homepage'))
